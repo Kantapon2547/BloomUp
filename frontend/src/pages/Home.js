933f064cb95f896a7d588a2ab6a8a8b664215d4d
Kantapon Hemmadhun <kantapon.hemmadhun@gmail.com>
2025-10-29 01:14:21 +0700

    fix: Synchronize package-lock.json with package.json

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -407,12 +407,11 @@ export default function Home({ user }) {
   useEffect(() => {
     const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
     gsap.set([".home-header", ".left-col > div", ".right-col > div"], { opacity: 1 });
-    
+
     tl.from(headerRef.current, { y: -50, opacity: 0, duration: 0.8 })
       .from(".left-col > div", { y: 30, opacity: 0, stagger: 0.15, duration: 0.6 }, "-=0.5")
       .from(".right-col > div", { y: 30, opacity: 0, stagger: 0.15, duration: 0.6 }, "-=0.7");
-   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // The error for this hook will now be ignored by the linter
+  }, [user?.email]);
 
   // Animate progress bar when percentage changes
   useEffect(() => {
